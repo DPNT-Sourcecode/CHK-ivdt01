f600184b785d555d461bca848ab4853eb95adc25
sbx_user1051 <sbx_user1051@169.254.189.213>
2019-11-10 21:40:40 +0000

    Sun Nov 10 21:40:40 UTC 2019

---
+++ test/solution_tests/CHK/test_chk.py
@@ -0,0 +1,7 @@
+from solutions.CHK import checkout_solution
+
+
+class TestSum():
+    def test_chk(self):
+        pass
+        #assert checkout_solution.checkout()
--- test/solution_tests/SUM/test_hlo.py
+++ test/solution_tests/SUM/test_hlo.py
@@ -2,7 +2,8 @@ from solutions.HLO import hello_solution
 
 
 class TestSum():
-    def test_sum(self):
+    def test_hello(self):
         assert hello_solution.hello("John") == "Hello, John!"
+
 
 
