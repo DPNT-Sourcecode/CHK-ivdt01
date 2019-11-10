a8a8abb429e54646bfab44d3af0ad1151887ed7b
sbx_user1051 <sbx_user1051@169.254.189.213>
2019-11-10 22:46:40 +0000

    Sun Nov 10 22:46:40 UTC 2019

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,7 +1,18 @@
 from solutions.CHK import checkout_solution
 
 
-class TestSum():
+class TestChk():
     def test_chk(self):
-        pass
-        #assert checkout_solution.checkout()
+        assert checkout_solution.checkout("XAA") == -1
+        assert checkout_solution.checkout("A A") == -1
+        assert checkout_solution.checkout("AA") == 100
+        assert checkout_solution.checkout("AAA") == 130
+        assert checkout_solution.checkout("AAAA") == 180
+        assert checkout_solution.checkout("AACAA") == 200
+        assert checkout_solution.checkout("AACCAA") == 220
+        assert checkout_solution.checkout("CAACCAA") == 240
+        assert checkout_solution.checkout("CAABCCAA") == 270
+        assert checkout_solution.checkout("CAABBCCAA") == 285
+        assert checkout_solution.checkout("CAADBBCCAA") == 300
+        assert checkout_solution.checkout("CAADBBBCCAA") == 330
+
