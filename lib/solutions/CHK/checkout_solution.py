05f1b87b29bcca73cefebc90d9411791c0d64966
sbx_user1051 <sbx_user1051@169.254.189.213>
2019-11-10 22:19:40 +0000

    Sun Nov 10 22:19:40 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,28 +1,30 @@
 
 
 class Sku:
-    def __init__(self, price, offer_unit=None, offer_price=None):
+    def __init__(self, item, price, offer_unit=None, offer_price=None):
+        self.item = item
         self.price = price
-        self.offer_unit=offer_unit
-        self.offer_price=offer_price
+        self.offer_unit = offer_unit
+        self.offer_price = offer_price
 
     def calculate_price(self,amount):
         sum = 0
         if self.offer_unit and self.offer_price:
-            sum += amount // self.offer_unit * self.offer_price # offers
-            sum += amount %  self.offer_unit * self.price       # non-offers
+            sum += amount // self.offer_unit * self.offer_price # in offer
+            sum += amount %  self.offer_unit * self.price       # not in offer
         else:
             sum += amount * self.price
         return sum
 
-skus = {
-    'A': Sku(50, 3, 130),
-    'B': Sku(30, 2, 45),
-    'C': Sku(20),
-    'D': Sku(15),
-}
+store_skus = [
+    Sku('A',50, 3, 130),
+    Sku('B',30, 2, 45),
+    Sku('C',20),
+    Sku('D',15),
+]
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    
+    for sku in store_skus:
+        
