ced787c4e6a6cbb4a51d477b1fc001882a6a1945
sbx_user1051 <sbx_user1051@169.254.189.213>
2019-11-10 23:17:55 +0000

    Sun Nov 10 23:17:55 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,19 +3,28 @@ from collections import Counter
 
 
 class Sku:
-    def __init__(self, item, price, offer_unit=None, offer_price=None):
+    def __init__(self, item, price, offer_unit=None, offer_price=None, extra_offer_unit=None, extra_offer_price=None):
         self.item = item
         self.price = price
         self.offer_unit = offer_unit
         self.offer_price = offer_price
+        self.extra_offer_unit = extra_offer_unit
+        self.extra_offer_price = extra_offer_price
 
     def calculate_price(self, amount):
         sum = 0
+
+        if self.extra_offer_unit and self.extra_offer_price:
+            in_extra_offer = amount // self.extra_offer_unit
+            sum += in_extra_offer * self.extra_offer_price
+            amount -= in_extra_offer
+
         if self.offer_unit and self.offer_price:
-            sum += amount // self.offer_unit * self.offer_price # in offers
-            sum += amount %  self.offer_unit * self.price       # not in offers
-        else:
-            sum += amount * self.price
+            in_offer = amount // self.offer_unit
+            sum += in_offer * self.offer_price
+            amount -= in_offer
+
+        sum += amount * self.price
         return sum
 
 
@@ -41,3 +50,4 @@ def checkout(skus):
     for item in item_counts:
         total += store_skus[item].calculate_price(item_counts[item])
     return total
+
