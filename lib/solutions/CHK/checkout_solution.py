03d519919e18afd0e119f529199dc0e3325b4e29
sbx_user1051 <sbx_user1051@169.254.189.213>
2019-11-11 00:40:32 +0000

    Mon Nov 11 00:40:32 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,7 +3,10 @@ from collections import Counter
 
 
 class Sku:
-    def __init__(self, item, price, offer_unit=None, offer_price=None, extra_offer_unit=None, extra_offer_price=None, free_item_for_two=None):
+    def __init__(self, item, price,
+                 offer_unit=None, offer_price=None,
+                 extra_offer_unit=None, extra_offer_price=None,
+                 free_item_for_two=None):
         self.item = item
         self.price = price
         self.offer_unit = offer_unit
@@ -63,6 +66,7 @@ def checkout(skus):
     for item in item_counts:
         total += store_skus[item].calculate_price(item_counts[item])
     return total
+
 
 
 
